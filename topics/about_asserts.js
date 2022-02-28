717c4fa11d8c17642d619b4d76945bc248106e1d
Tracy B Porter <tbporter@gmail.com>
2022-02-28 15:20:21 -0600

    tracy - learning qunit. not terribly valuable

--- topics/about_asserts.js
+++ topics/about_asserts.js
@@ -1,14 +1,14 @@
 
 module("About Asserts (topics/about_asserts.js)");
 
-test("ok", function() {
-    ok(__ === true, 'what will satisfy the ok assertion?');
+test("ok assertTrue", function () {
+    ok(true === true, 'true is true');
 });
 
-test("not ok", function() {
-    ok(__ === false, 'what is a false value?');
+test("not ok", function () {
+    ok(false === false, 'false is false');
 });
 
-test("equal", function() {
-    equal(__, 1 + 1, 'what will satisfy the equal assertion?');
+test("assert equal", function () {
+    equal(2, 1 + 1, 'what will satisfy the equal assertion?');
 });
