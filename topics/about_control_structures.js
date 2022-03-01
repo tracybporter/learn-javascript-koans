cc26ba22244bbb789767002182c0145820e50f68
Tracy B Porter <tbporter@gmail.com>
2022-03-01 08:57:58 -0600

    tracy - working with stuff

--- topics/about_control_structures.js
+++ topics/about_control_structures.js
@@ -1,36 +1,39 @@
 module("About Control Structures (topics/about_control_structures.js)");
 
-test("if", function() {
+test("if", function () {
 	var isPositive = false;
 	if (2 > 0) {
 		isPositive = true;
 	}
-	equal(__, isPositive,  'what is the value of isPositive?');
+	ok(isPositive, 'what is the value of isPositive?');
 });
 
-test("for", function() {
+test("for", function () {
 	var counter = 10;
 	for (var i = 1; i <= 3; i++) {
 		counter = counter + i;
 	}
-	equal(__, counter, 'what is the value of counter?');
+	equal(11 + 2 + 3, counter, 'what is the value of counter?');
 });
 
-test("for in", function() {
+test("for in", function () {
 	// this syntax will be explained in about objects
 	var person = {
 		name: "Amory Blaine",
 		age: 102
 	};
-	var result = "";
+	var propertyNames = "";
+	var propertyValues = "";
 	// for in enumerates the property names of an object
 	for (var property_name in person) {
-  		result = result + property_name;
+		propertyNames += property_name;
+		propertyValues += person[property_name];
 	}
-	equal(__, result, 'what is the value of result?');
+	equal('nameage', propertyNames, 'what is the value of result?');
+	equal('Amory Blaine102', propertyValues, 'what is the value of result?');
 });
 
-test("ternary operator", function() {
+test("ternary operator", function () {
 	var fruit = true ? "apple" : "orange";
 	equal(__, fruit, 'what is the value of fruit?');
 
@@ -38,36 +41,36 @@ test("ternary operator", function() {
 	equal(__, fruit, 'now what is the value of fruit?');
 });
 
-test("switch", function() {
+test("switch", function () {
 	var result = 0;
 	switch (2) {
 		case 1:
 			result = 1;
 			break;
-		case 1+1:
+		case 1 + 1:
 			result = 2;
 			break;
 	}
 	equal(__, result, 'what is the value of result?');
 });
 
-test("switch default case", function() {
-    var result = "Pippin";
-    switch ("m") {
-        case "f":
-            result = "Frodo";
-            break;
-        case "s":
-            result = "Samwise";
-                break;
-        default:
-            result = "Merry";
-            break;
-    }
-    equal(__, result, 'what is the value of result?');
+test("switch default case", function () {
+	var result = "Pippin";
+	switch ("m") {
+		case "f":
+			result = "Frodo";
+			break;
+		case "s":
+			result = "Samwise";
+			break;
+		default:
+			result = "Merry";
+			break;
+	}
+	equal(__, result, 'what is the value of result?');
 });
 
-test("null coalescing", function() {
-    var result = null || "a value";
-    equal(__, result, 'what is the value of result?');
+test("null coalescing", function () {
+	var result = null || "a value";
+	equal(__, result, 'what is the value of result?');
 });
